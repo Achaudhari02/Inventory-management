bd0819a5b116a52ed0be61871ae161d610b25e27
Achaudhari02 <ashutoshc018@gmail.com>
2025-12-03 22:31:55 -0500

    created signUp view, signUpForm

--- micro_SaaS/inventory/views.py
+++ micro_SaaS/inventory/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages 
+from .forms import SignUpForm 
 # Create your views here.
+
+def signup_view(request):
+
+    if request.user.is_authenticated:
+        redirect('dashboard')
+    
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account was successfully created')
+            return redirect('dashboard')
+    else: 
+        form = SignUpForm()
+    return render(request, 'inventory/signup.html', {'form': form})
+
+
